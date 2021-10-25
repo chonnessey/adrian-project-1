b7d0940bea94d9530f7015481cfb5bd0134d25d9
Adrian Camacho <adriancamacho18@gmail.com>
2021-10-25 15:16:47 -0700

    add scroll animation to logo

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -3,3 +3,13 @@ import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 gsap.registerPlugin(ScrollTrigger);
+
+gsap.to('.fav-img', {
+  scrollTrigger: {
+    trigger: '.fav-img',
+    toggleActions: 'play none none none'
+  },
+  x: 400,
+  rotation: 360,
+  duration: 3
+})
