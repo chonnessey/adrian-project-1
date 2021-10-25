dc8c34d5e0b2a028a6f9cd24d3fc78e0d51321f3
Adrian Camacho <adriancamacho18@gmail.com>
2021-10-25 14:59:21 -0700

    add initial styling to logo

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -1,8 +1,5 @@
 // Add your scripts here
-import {
-  gsap
-} from "gsap";
-import {
-  ScrollTrigger
-} from "gsap/ScrollTrigger";
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
 gsap.registerPlugin(ScrollTrigger);
