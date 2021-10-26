b03333aa0e8422b0bd66b7d1a6aa6732ecce019a
Adrian Camacho <adriancamacho18@gmail.com>
2021-10-25 22:07:41 -0700

    fix rotations to be smoother on scroll

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -21,7 +21,7 @@ let tl = gsap.timeline({
     trigger: '.container',
     pin: true,   // pin the trigger element while active
     start: "top top", // when the top of the trigger hits the top of the viewport
-    end: "+=6000", // end after scrolling 500px beyond the start
+    end: "+=5000", // end after scrolling 500px beyond the start
     scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
     snap: {
       snapTo: "labels", // snap to the closest label in the timeline
@@ -34,16 +34,17 @@ let tl = gsap.timeline({
 
 // add animations and labels to the timeline
 tl
+.to('.fav-img', {duration: 1, scale: 2, rotation: 45})
 .addLabel("color")
-.to(".fav-img", {scale: 0.3, rotation:45})
+.to(".fav-img", {scale: 1.7, rotation:90, duration: 1})
 .to(".container", {backgroundColor: "#28a92b"})
 .addLabel("second-color")
-.to('.fav-img', {scale: 1.2, rotation:180})
+.to('.fav-img', {scale: 1.4, rotation:135, duration: 1})
 .to('.container', {backgroundColor: 'blue'})
 .addLabel("third-color")
-.to('.fav-img', {scale: 0.5, rotation:45})
+.to('.fav-img', {scale: 1, rotation:180, duration: 1})
 .to('.container', {backgroundColor: '#cc34eb'})
 .addLabel("fourth-color")
-.to('.fav-img', {rotation: 360, duration:3})
+.to('.fav-img', {rotation: 360, duration:1})
 .to('.container', {backgroundColor: 'yellow'})
-.to('.fav-img', {xPercent: 50, duration: 2, scale: 1.2, })
+.to('.fav-img', {xPercent: 50, duration: 1, scale: 2, })
